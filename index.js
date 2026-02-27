059deccadca022fd642e19722694907a1620188e
Dev php <251776897+jolamu200@users.noreply.github.com>
2026-02-27 04:16:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ async function loadGiftedSession() {
     if (!fs.existsSync(sessionDir)) fs.mkdirSync(sessionDir, { recursive: true });
     if (fs.existsSync(credsPath)) return true;
 
-    if (config.SESSION_ID && config.SESSION_ID.startsWith("NOVA~")) {
-        const compressedBase64 = config.SESSION_ID.substring("NOVA~".length);
+    if (config.SESSION_ID && config.SESSION_ID.startsWith("NOVA;;;")) {
+        const compressedBase64 = config.SESSION_ID.substring("NOVA;;;".length);
         try {
             const compressedBuffer = Buffer.from(compressedBase64, 'base64');
             if (compressedBuffer[0] === 0x1f && compressedBuffer[1] === 0x8b) {
